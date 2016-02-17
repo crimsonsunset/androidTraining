3186fa819b2fddb061061b4be25363387de6c667
Joseph Sangiorgio <jsangiorgio@marvel.com>
2016-02-17 14:34:03 -0500

    added menu

--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import com.garagze.event.domain.SaleEvent;
@@ -45,5 +48,40 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //stuff for menu
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.mi_add_event:
+                addEvent();
+                return true;
+            case R.id.mi_show_map:
+                showMap();
+                return true;
+            case R.id.mi_prefs:
+                showPrefs();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private void showPrefs(){
+        Log.v(TAG, "Running showPrefs method.");
+    }
+    private void showMap() {
+        Log.v(TAG, "Running showMap method.");
+    }
+    private void addEvent() {
+        Log.v(TAG, "Running addEvent method.");
+    }
 
 }
